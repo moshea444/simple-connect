37d4fd59c37b67ba7fdaa1a962a685b49fa97d96
Michael O'Shea <moshea444@gmail.com>
2016-09-26 13:47:43 -0600

    Added toolbar.

--- SimpleConnect/app/src/main/java/com/simpleconnect/app/HomeActivity.java
+++ SimpleConnect/app/src/main/java/com/simpleconnect/app/HomeActivity.java
@@ -3,6 +3,7 @@ package com.simpleconnect.app;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,7 +15,9 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Toast.makeText(this, "HELLO!", Toast.LENGTH_SHORT).show();
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        
+        setSupportActionBar(myToolbar);
 
         Button bEvent = (Button)findViewById(R.id.b_events);
         bEvent.setOnClickListener(new View.OnClickListener() {
